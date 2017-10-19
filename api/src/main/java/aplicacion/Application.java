3e4f1873e7943b5da230e3157c1bd878cdbf258d
federico <federico.x.bernardi.-nd@disney.com>
2017-10-18 22:39:10 -0300

    Agregando creacion de BD y limpiando config de Hibernate

--- api/src/main/java/aplicacion/Application.java
+++ api/src/main/java/aplicacion/Application.java
@@ -3,11 +3,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import aplicacion.*;
+import conexion.Conexion;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-    	SpringApplication.run(Application.class, args);
+        // Nos aseguramos que se cree la base de datos cuando se
+        // ejecuta la aplicacion.
+        Conexion cn = new Conexion();
+        cn.abrirConexion();
+        cn.cerrarConexion();
+        SpringApplication.run(Application.class, args);
     }
 }
