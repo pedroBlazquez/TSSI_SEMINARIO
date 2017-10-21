c59153616cd7e8fc11ddc7c2134b1c2844de00ab
Pedro Blazquez <p.e.blazquez@gmail.com>
2017-10-20 21:22:30 -0300

    Added SASS

--- vista/app/src/index.js
+++ vista/app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import './styles/index.css';
 import App from './views/App';
 import registerServiceWorker from './utils/registerServiceWorker';
