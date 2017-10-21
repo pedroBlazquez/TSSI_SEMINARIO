39da14e77d66795281365a0ad0f70cc8437a033c
Pedro Blazquez <p.e.blazquez@gmail.com>
2017-10-20 21:28:43 -0300

    Removed placeholders from react-create-app

--- vista/app/src/views/App.js
+++ vista/app/src/views/App.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
-import '../styles/App.css';
 import {Button} from 'antd';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Button >
-          {'Some button'}
-        </Button>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. Hola
-        </p>
+        <Button>Bienvenido</Button>
       </div>
     );
   }
