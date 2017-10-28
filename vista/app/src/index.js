43657606b29cfcc40195a01ab36acfde873c5751
Pedro Blazquez <p.e.blazquez@gmail.com>
2017-10-28 18:14:54 -0300

    Added Sagas

--- vista/app/src/index.js
+++ vista/app/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from './reducers';
+import rootSaga from './sagas';
 import Route from './routes';
 import registerServiceWorker from './utils/registerServiceWorker';
 
@@ -11,7 +13,10 @@ import 'antd/dist/antd.css';
 import './styles/index.css';
 import './styles/errors.css';
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
