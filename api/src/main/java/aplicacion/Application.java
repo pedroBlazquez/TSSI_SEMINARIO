81b2f21a6ef199df3c5bcea1334fd61e5ffdfaa2
federico <federico.x.bernardi.-nd@disney.com>
2017-10-30 01:57:25 -0300

    First commit

--- api/src/main/java/aplicacion/Application.java
+++ api/src/main/java/aplicacion/Application.java
@@ -1,6 +1,8 @@
 package aplicacion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import aplicacion.*;
 import conexion.Conexion;
@@ -8,12 +10,17 @@ import conexion.Conexion;
 @SpringBootApplication
 public class Application {
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
     public static void main(String[] args) {
         // Nos aseguramos que se cree la base de datos cuando se
         // ejecuta la aplicacion.
-        Conexion cn = new Conexion();
-        cn.abrirConexion();
-        cn.cerrarConexion();
+        //Conexion cn = new Conexion();
+        //cn.abrirConexion();
+        //cn.cerrarConexion();
         SpringApplication.run(Application.class, args);
     }
 }
---
+++ api/src/main/java/aplicacion/autenticacion/ApplicationUserRepository.java
@@ -0,0 +1,9 @@
+package aplicacion.autenticacion;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+    ApplicationUser findByUsername(String username);
+}
