a79946ee7cbae2e71cdf7daf9a872a8268f93392
federico <federico.x.bernardi.-nd@disney.com>
2017-11-06 22:00:20 -0300

    generos

--- default-data.sql
+++ default-data.sql
@@ -9,3 +9,11 @@ INSERT INTO integrantesroles(descripcion) VALUES("Vocalista");
 INSERT INTO integrantesroles(descripcion) VALUES("Gutarrista");
 INSERT INTO integrantesroles(descripcion) VALUES("Bajista");
 INSERT INTO integrantesroles(descripcion) VALUES("Baterista");
+
+INSERT INTO generos(descripcion) VALUES("Rock");
+INSERT INTO generos(descripcion) VALUES("Pop");
+INSERT INTO generos(descripcion) VALUES("Electronica");
+INSERT INTO generos(descripcion) VALUES("Rap");
+INSERT INTO generos(descripcion) VALUES("Cumbia");
+INSERT INTO generos(descripcion) VALUES("Reggaeton");
+INSERT INTO generos(descripcion) VALUES("Heavy Metal");
