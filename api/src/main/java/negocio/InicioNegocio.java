114ed2666039dc145f235849a5126b6b4c51c737
Branko <branlip@gmail.com>
2017-11-10 14:28:46 -0300

    fix buscar novedades

--- api/src/main/java/negocio/InicioNegocio.java
+++ api/src/main/java/negocio/InicioNegocio.java
@@ -44,6 +44,7 @@ public class InicioNegocio {
                 "WHERE s.idSeguidos.seguidor.id = "+usuario.getId()+
                 " and s.idSeguidos.seguido.usuarioTipo.id != " + 2); //CAMBIAR NUMERO ACORDE AL USUARIO TIPO OYENTE
             
+            
             if(!artistas_seguidos.isEmpty())
             {
                 //fecha actual
@@ -93,8 +94,8 @@ public class InicioNegocio {
                 query_in_generos.deleteCharAt(query_in_generos.length() - 1);
                 
                 //busco novedades para Discos y Canciones de los generos que le gustan al usuario
-                general_list.addAll(CancionNegocio.setData(cn.getListQuery("select gc.idGeneroCancion.cancion from GeneroCancion gc WHERE gc.idGeneroCancion.cancion.artista.id not in ("+query_artistas_seguidos+") and gc.idGeneroCancion.genero.id in ("+query_in_generos+") and gc.idGeneroCancion.cancion.fechaPublicacion > '"+date_novedades+"'"), usermail));
-                general_list.addAll(DiscoNegocio.setData(cn.getListQuery("select gd.idGeneroDisco.disco from GeneroDisco gd WHERE gd.idGeneroDisco.disco.artista.id not in ("+query_artistas_seguidos+") and gd.idGeneroDisco.genero.id in ("+query_in_generos+") and gd.idGeneroDisco.disco.fechaPublicacion > '"+date_novedades+"'"), usermail));
+                general_list.addAll(CancionNegocio.setData(cn.getListQuery("select gc.idGeneroCancion.cancion from GeneroCancion gc WHERE gc.idGeneroCancion.cancion.artista.id not in ("+query_artistas_seguidos+") and gc.idGeneroCancion.genero.id in ("+query_in_generos+") and gc.idGeneroCancion.cancion.fechaPublicacion > '"+date_novedades+"'",20), usermail));
+                general_list.addAll(DiscoNegocio.setData(cn.getListQuery("select gd.idGeneroDisco.disco from GeneroDisco gd WHERE gd.idGeneroDisco.disco.artista.id not in ("+query_artistas_seguidos+") and gd.idGeneroDisco.genero.id in ("+query_in_generos+") and gd.idGeneroDisco.disco.fechaPublicacion > '"+date_novedades+"'",20), usermail));
                 
                 //obtengo artistas del genero que le gustan al usuario, pero que no sigue
                 List<Artista> artistas_genero = cn.getListQuery("select ga.idGeneroArtista.artista from GeneroArtista ga WHERE ga.idGeneroArtista.artista.id not in ("+query_artistas_seguidos+") and ga.idGeneroArtista.genero.id in ("+query_in_generos+")");
@@ -107,9 +108,9 @@ public class InicioNegocio {
                     query_artistas_genero.deleteCharAt(query_artistas_genero.length() - 1);
                     
                     //busco novedades para Album, Publicacion y Evento, de artistas del genero, que no sean seguidos por el usuario
-                    general_list.addAll(AlbumNegocio.setData(cn.getListQuery("from Album WHERE artista.id in ("+query_artistas_genero+") and fechaPublicacion > '"+date_novedades+"'"), usermail));
-                    general_list.addAll(PublicacionNegocio.setData(cn.getListQuery("from Publicacion WHERE artista.id in ("+query_artistas_genero+") and fechaPublicacion > '"+date_novedades+"'"), usermail));
-                    general_list.addAll(EventoNegocio.setData(cn.getListQuery("from Evento WHERE artista.id in ("+query_artistas_genero+") and fechaPublicacion > '"+date_novedades+"' and fechaEvento > '"+date_now+"'"), usermail));
+                    general_list.addAll(AlbumNegocio.setData(cn.getListQuery("from Album WHERE artista.id in ("+query_artistas_genero+") and fechaPublicacion > '"+date_novedades+"'",20), usermail));
+                    general_list.addAll(PublicacionNegocio.setData(cn.getListQuery("from Publicacion WHERE artista.id in ("+query_artistas_genero+") and fechaPublicacion > '"+date_novedades+"'",20), usermail));
+                    general_list.addAll(EventoNegocio.setData(cn.getListQuery("from Evento WHERE artista.id in ("+query_artistas_genero+") and fechaPublicacion > '"+date_novedades+"' and fechaEvento > '"+date_now+"'",20), usermail));
                     
                     //agrego aleatoriamente 3 artistas que de los generos que le gustan al usuario
                     List<JSONObject> artistas = ArtistaNegocio.setData(artistas_genero, usermail);
@@ -152,10 +153,130 @@ public class InicioNegocio {
             List<Usuario> usuarios = cn.getListQuery("from modelos.Usuario WHERE mail = '"+usermail+"'");
             Usuario usuario = usuarios.get(0);
             
+            //fecha actual
+            String date_now = Tools.DateFormatter(new Date());
+            
+            
+            //creo lista para devolver
+            List<JSONObject> return_list = new ArrayList<JSONObject>();
+            //lista de prioridad (aparece primero)
+            List<JSONObject> priority_list = new ArrayList<JSONObject>();
+            //lista secundaria
+            List<JSONObject> general_list = new ArrayList<JSONObject>();
+            
+            String filtro_artista_directo = "";
+            if(!artista.equals(""))
+                filtro_artista_directo = " and artista.nombreFantasia like '%"+artista+"%' ";
+            String filtro_fechaEvento = " > '"+date_now+"' ";
+            if(fecha != null)
+            {
+                String signo = ">";
+                if(desdehasta.equals("Desde"))
+                    signo = "<";
+                filtro_fechaEvento = " "+signo+" '"+Tools.DateFormatter(fecha)+"' ";
+            }
+            String st_nombre = "";
+            String st_publicacion = "";
+            String busqueda_usuario = "";
+            if(!busqueda.equals("") || !busqueda.isEmpty())
+            {
+                st_nombre = " and nombre like '%"+busqueda+"%' ";
+                st_publicacion = " and texto like '%"+busqueda+"%' ";
+                
+                String nombre = busqueda;
+                String apellido = "";
+                if(busqueda.indexOf(' ') > 1)
+                {
+                    nombre = busqueda.substring(0, busqueda.indexOf(' '));
+                    apellido = busqueda.substring(busqueda.indexOf(' ')+1);
+                }
+                
+                if(!apellido.equals(""))
+                {
+                    busqueda_usuario = " nombre like '%"+nombre+"%' and apellido like '%"+apellido+"%' ";
+                }
+                else
+                {
+                    busqueda_usuario = " (nombre like '%"+nombre+"%' or apellido like '%"+nombre+"%' )";
+                }
+                 
+            }
+            String filtro_direccion = "";
+            if(!direccion.equals("") || !direccion.isEmpty())
+            {
+                filtro_direccion = " and direccion like %"+direccion+"% ";
+            }
+            
+            if(!genero.equals("") || !genero.isEmpty())
+            {
+                List<Genero> list_generos = cn.getListQuery("from modelos.Genero WHERE descripcion = '"+genero+"'");
+                int idGenero = list_generos.get(0).getId();
+                
+                String filtro_cancion = "";
+                String filtro_disco = "";
+                if(!artista.equals("") || !artista.isEmpty())
+                {
+                    filtro_cancion = " and gd.idGeneroDisco.disco.artista.nombreFantasia like '%"+artista+"%'";
+                    filtro_disco = " and gd.idGeneroDisco.disco.artista.nombreFantasia like '%"+artista+"%'";
+                }
+                if(!busqueda.equals("") || !busqueda.isEmpty())
+                {
+                    filtro_cancion = " and nombre like '%"+busqueda+"%' ";
+                    filtro_disco = " and texto like '%"+busqueda+"%' ";
+                }
+                if(direccion.equals("") || direccion.isEmpty())
+                {
+                    priority_list.addAll(CancionNegocio.setData(cn.getListQuery("select gc.idGeneroCancion.cancion from GeneroCancion gc WHERE gc.idGeneroCancion.genero.id = "+idGenero+" "+filtro_cancion+" order by gc.idGeneroCancion.cancion.fechaPublicacion desc",20), usermail));
+                    priority_list.addAll(DiscoNegocio.setData(cn.getListQuery("select gd.idGeneroDisco.disco from GeneroDisco gd WHERE gd.idGeneroDisco.genero.id = "+idGenero+" "+filtro_disco+" order by gd.idGeneroDisco.disco.fechaPublicacion desc",20), usermail));
+                }
+                List<Artista> artistas_genero = cn.getListQuery("select ga.idGeneroArtista.artista from GeneroArtista ga WHERE ga.idGeneroArtista.genero.id = "+idGenero+" "+filtro_artista_directo);
+                if(!artistas_genero.isEmpty() || !artistas_genero.isEmpty())
+                {
+                    //armo query para filtrar por artistas de los generos
+                    StringBuilder query_artistas_genero = new StringBuilder();
+                    for (Artista a : artistas_genero)
+                        query_artistas_genero.append(a.getId()).append(",");
+                    query_artistas_genero.deleteCharAt(query_artistas_genero.length() - 1);
+                    if(direccion.equals("") || direccion.isEmpty())
+                    {
+                        priority_list.addAll(AlbumNegocio.setData(cn.getListQuery("from Album WHERE artista.id in ("+query_artistas_genero+") "+st_nombre+" order by fechaPublicacion desc ",20), usermail));
+                        priority_list.addAll(PublicacionNegocio.setData(cn.getListQuery("from Publicacion WHERE artista.id in ("+query_artistas_genero+") "+st_publicacion+" order by fechaPublicacion desc",20), usermail));
+                    }
+                    priority_list.addAll(EventoNegocio.setData(cn.getListQuery("from Evento WHERE artista.id in ("+query_artistas_genero+") "+st_nombre+filtro_direccion+" and fechaEvento "+filtro_fechaEvento+" order by fechaEvento desc",20), usermail));
+                    
+                }
+            }
+            if(direccion.equals("") || direccion.isEmpty())
+            {
+                String filtros = st_nombre+filtro_artista_directo;
+                if(!filtros.equals("") || !filtros.isEmpty())
+                {
+                    filtros = filtros.replace(" and ", "");
+                    priority_list.addAll(CancionNegocio.setData(cn.getListQuery("from Cancion WHERE  "+filtros), usermail));
+                    priority_list.addAll(DiscoNegocio.setData(cn.getListQuery("from Disco WHERE  "+filtros), usermail));
+                    priority_list.addAll(AlbumNegocio.setData(cn.getListQuery("from Album WHERE  "+filtros), usermail));
+                }
+                
+                filtros = st_publicacion+filtro_artista_directo;
+                if(!filtros.equals("") || !filtros.isEmpty())
+                {
+                    filtros = filtros.replace(" and ", "");
+                    priority_list.addAll(PublicacionNegocio.setData(cn.getListQuery("from Publicacion WHERE  "+filtros), usermail));
+                }
+                
+                if(!busqueda_usuario.equals(""))
+                {
+                    priority_list.addAll(UsuarioNegocio.setData(cn.getListQuery("from Usuario WHERE "+busqueda_usuario),usermail));
+                }
+            }
+            priority_list.addAll(EventoNegocio.setData(cn.getListQuery("from Evento WHERE fechaEvento "+filtro_fechaEvento+" "+st_nombre+filtro_artista_directo+filtro_direccion+" order by fechaEvento desc"), usermail));
+            
+                
+                
             
             
             cn.cerrarConexion();
-            return new ResponseEntity<Object>(usuarios,HttpStatus.NOT_MODIFIED);
+            return new ResponseEntity<Object>(priority_list.toString(),HttpStatus.OK);
         }catch(Exception e)
         {
             e.printStackTrace();
@@ -164,3 +285,4 @@ public class InicioNegocio {
     }
   
 }
+
