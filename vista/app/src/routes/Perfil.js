71eda766ab52e6aea8e3ebc36b796132a23987a2
Pedro Blazquez <p.e.blazquez@gmail.com>
2017-11-20 11:58:57 -0300

    Actualizado el estilo donde se mostrarán todos los contenidos

--- vista/app/src/routes/Perfil.js
+++ vista/app/src/routes/Perfil.js
@@ -6,6 +6,8 @@ import AuthRoute from './AuthRoute';
 import PerfilContainer from '../containers/PerfilContainer';
 import NovedadesPerfil from '../containers/NovedadesPerfil';
 import AdministrarContenido from '../containers/AdministrarContenidoContainer';
+import CancionesPerfil from '../containers/CancionesPerfil';
+import DiscosPerfil from '../containers/DiscosPerfil';
 
 const Perfil = () => (
   <PerfilContainer>
@@ -14,6 +16,8 @@ const Perfil = () => (
       <Route path={'/perfil/:profileId/seguidores'} component={() => (<div>{'Aca van los seguidores'}</div>)}/>
       <Route path={'/perfil/:profileId/seguidos'} component={() => (<div>{'Aca van los seguidos'}</div>)}/>
       <Route path={'/perfil/:profileId/listas'} component={() => (<div>{'Aca van las listas de reproduccion'}</div>)}/>
+      <Route path={'/perfil/:profileId/canciones'} component={CancionesPerfil}/>
+      <Route path={'/perfil/:profileId/discos'} component={DiscosPerfil}/>
       <AuthRoute
         path={'/perfil/:profileId/administrar'}
         exact
