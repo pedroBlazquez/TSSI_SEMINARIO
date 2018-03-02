29db373ee0daf1b0c11c22bf05c6fae1aaf4f9f1
Pedro Blazquez <p.e.blazquez@gmail.com>
2018-03-02 16:08:02 -0300

    Filtrado publicaciones "descompartidas" de perfil de usuario

--- vista/app/src/reducers/perfil/publicaciones.js
+++ vista/app/src/reducers/perfil/publicaciones.js
@@ -1,9 +1,13 @@
-import {PERFIL_PUBLICACIONES, PERFIL_RESTORE} from '../../actions/types';
+import {PERFIL_PUBLICACIONES, PERFIL_RESTORE, SEND_COMPARTIR} from '../../actions/types';
 
 const initialState = [];
 
 export default function (state = initialState, action) {
   switch(action.type) {
+    case SEND_COMPARTIR:
+      return state.filter(p => {
+        return !(p.object_type === action.typeRecord && p.id === action.id && p.compartido);
+      });
     case PERFIL_PUBLICACIONES:
       return action.publicaciones ? action.publicaciones.map(p => {
         const objectType = p.object_type.toLowerCase();
